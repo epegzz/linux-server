ef16cde1fbf7fdf179f60569b3295493aa0f16a7
Daniel Schäfer <epegzz@gmail.com>
2021-06-19 14:28:10 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,3 +15,9 @@ cat << EOF >> ~/.zshrc
 # Load defaults from epegzz/linux-server 
 . ~/.config/linux-server/.zshrc
 EOF
+
+## Install EternalTerminal (Ubuntu)
+echo "deb https://github.com/MisterTea/debian-et/raw/master/debian-source/ buster main" | sudo tee -a /etc/apt/sources.list.d/et.list
+curl -sSL https://github.com/MisterTea/debian-et/raw/master/et.gpg | sudo apt-key add -
+sudo apt update
+sudo apt install et
